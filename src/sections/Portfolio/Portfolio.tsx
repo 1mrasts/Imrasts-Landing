59c4d038e7867b18acaa77a2ae344d30a61bfdb6
1mrasts <olgaakshidi.ru@gmail.com>
2026-04-15 21:20:32 +0300

    Fix Portfolio image

--- src/sections/Portfolio/Portfolio.tsx
+++ src/sections/Portfolio/Portfolio.tsx
@@ -1,23 +1,14 @@
 import type { TTranslation } from '../../App'
 import nft_image_big from '../../assets/Portfolio/nft-big.png'
-import nft_image from '../../assets/Portfolio/nft.png'
 import nft from '../../assets/Portfolio/nft.svg'
 import payday_image_big from '../../assets/Portfolio/payday-big.png'
-import payday_image from '../../assets/Portfolio/payday.png'
 import payday from '../../assets/Portfolio/payday.svg'
 import todo_image_big from '../../assets/Portfolio/todo-big.png'
-import todo_image from '../../assets/Portfolio/todo.png'
 import todo from '../../assets/Portfolio/todo.svg'
 import topic from '../../assets/Portfolio/topic.svg'
 import styles from './Portfolio.module.scss'
 
-export function Portfolio({
-	width,
-	lang,
-}: {
-	width: number
-	lang: TTranslation
-}) {
+export function Portfolio({ lang }: { width: number; lang: TTranslation }) {
 	return (
 		<div className={`${styles.portfolio} mt-100`}>
 			<div>
@@ -35,7 +26,7 @@ export function Portfolio({
 					<h5>NFT-Marketplace-Landing</h5>
 				</div>
 				<a href='https://github.com/1mrasts/Nft-Marketplace-Landing'>
-					<img src={width < 1100 ? nft_image : nft_image_big} alt='' />
+					<img src={nft_image_big} alt='' />
 				</a>
 			</div>
 			<div className={styles['portfolio__block']}>
@@ -44,7 +35,7 @@ export function Portfolio({
 					<h5>Payday-App</h5>
 				</div>
 				<a href='https://github.com/1mrasts/Payday-App'>
-					<img src={width < 1100 ? payday_image : payday_image_big} alt='' />
+					<img src={payday_image_big} alt='' />
 				</a>
 			</div>
 			<div className={styles['portfolio__block']}>
@@ -53,7 +44,7 @@ export function Portfolio({
 					<h5>Todo-App-With-Redux</h5>
 				</div>
 				<a href='https://github.com/1mrasts/Todo-App-with-Redux'>
-					<img src={width < 1100 ? todo_image : todo_image_big} alt='' />
+					<img src={todo_image_big} alt='' />
 				</a>
 			</div>
 		</div>
